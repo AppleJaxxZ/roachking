b1fdfeed528ab99264f39c60b5dff2af1ea7e253
reactnodejs32 <reactnodejs32@gmail.com>
2021-07-01 23:31:03 -0400

    setup stripe

--- roachking/src/pages/checkout.pages.jsx
+++ roachking/src/pages/checkout.pages.jsx
@@ -1,50 +1,81 @@
 import React from "react";
-import './checkout.styles.scss'
+import "./checkout.styles.scss";
 
 import { createStructuredSelector } from "reselect";
-import {
-    selectCartItems,
-    selectCartTotal,
-} from "../Redux/cart/cart.selectors";
+import { selectCartItems, selectCartTotal } from "../Redux/cart/cart.selectors";
 import { connect } from "react-redux";
-import CheckoutItem from '../components/checkout-item/checkout-item.components'
-import StripeCheckoutButton from "../components/stripecheckoutbutton/stripecheckoutbutton.component";
 
-const CheckoutPage = ({ cartItems, total }) => (
+import { useStripe } from "@stripe/react-stripe-js";
+import CheckoutItem from "../components/checkout-item/checkout-item.components";
+// import StripeCheckoutButton from "../components/stripecheckoutbutton/stripecheckoutbutton.component";
+import axios from "axios";
+
+const CheckoutPage = ({ cartItems, total }) => {
+  const stripe = useStripe();
+  const payNow = async () => {
+    const {
+      data: { sessionId },
+    } = await axios.post("http://localhost:8080/create-checkout-session", {
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: "usd",
+            unit_amount: 50,
+            product_data: {
+              name: "Roach",
+              description: "Amazing and delicious",
+              images: ["https://i.ibb.co/DRvM2RM/med-Dubia-Five-Eights.png"],
+            },
+          },
+        },
+      ],
+      customer_email: "smackanalex@gmail.com",
+    });
+    const { error } = await stripe.redirectToCheckout({
+      sessionId,
+    });
+    if (error) {
+      console.log(error);
+    }
+  };
+  return (
     <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">
-                <span>Product</span>
-            </div>
-            <div className="header-block">
-                <span>Description</span>
-            </div>
-            <div className="header-block">
-                <span>Quantity</span>
-            </div>
-            <div className="header-block">
-                <span>Price</span>
-            </div>
-            <div className="header-block">
-                <span>Remove</span>
-            </div>
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
         </div>
-
-        {cartItems.map((cartItem) => (
-            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-        ))}
-        <div className="total">TOTAL: ${total}</div>
-
-        <div className="test-warning">
-            *Please use the following test creditcard numbers for payments* <br />
-            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        <div className="header-block">
+          <span>Description</span>
         </div>
-        <StripeCheckoutButton price={total} />
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">TOTAL: ${total}</div>
+
+      <div className="test-warning">
+        *Please use the following test creditcard numbers for payments* <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <button onClick={() => payNow()}>Pay now</button>
+      {/* <StripeCheckoutButton price={total} /> */}
     </div>
-);
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    total: selectCartTotal,
+  cartItems: selectCartItems,
+  total: selectCartTotal,
 });
 export default connect(mapStateToProps)(CheckoutPage);
--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config({ path: './.env' });
-const createCheckoutSession = require('./api//checkout');
+const express = require("express");
+const cors = require("cors");
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+const createCheckoutSession = require("./api/checkout");
 
 const app = express();
 const port = 8080;
 
-
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get("/", (req, res) => res.send("Hello World"));
 
-app.post('/create-checkout-session', createCheckoutSession);
+app.post("/create-checkout-session", createCheckoutSession);
 
-app.listen(port, () => console.log('server listening on port', port));
-
+app.listen(port, () => console.log("server listening on port", port));
