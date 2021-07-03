197cd69832a3847787970a136c4c7cd8a582837e
reactnodejs32 <reactnodejs32@gmail.com>
2021-07-02 21:28:45 -0400

    fixed an alex issue

--- server/index.js
+++ server/index.js
@@ -45,10 +45,10 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 });
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
 
