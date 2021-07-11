ed7ea8e86e29a83f5f0536af9b5f439ffaaa7b40
AppleJaxxZ <smackanalex@gmail.com>
2021-07-11 09:48:03 -0400

    Changed font sizes and font-family

--- client/src/components/Contact/Contact.components.jsx
+++ client/src/components/Contact/Contact.components.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Contact.styles.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
@@ -8,9 +12,24 @@ const Contact = () => {
         <div>
           <p>Phone: +1(484)-309-3456</p>
           <p>Email: reptilegoodies@yahoo.com</p>
-          <i>Instagram</i>
+          <a href="tel:+1(484)-309-3456">
+            <FontAwesomeIcon id="icon" icon={faPhone} size="3x" />
+          </a>
+          <Link
+            to="#"
+            onClick={(e) => {
+              window.open("mailto:reptilegoodies@yahoo.com");
+              e.preventDefault();
+            }}
+          >
+            <FontAwesomeIcon id="icon" icon={faEnvelope} size="3x" />
+          </Link>
+          <a href="https://www.instagram.com/dragonfeeders" target="_blank">
+            <FontAwesomeIcon id="icon" icon={["fab", "instagram"]} size="3x" />
+          </a>
         </div>
       </div>
+
       <div className="paragraphs">
         <h5>
           Please feel free to give us call ANYDAY between 10am and 7pm Eastern
