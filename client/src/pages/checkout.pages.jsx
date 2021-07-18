c3cd8338442cb48b9b483879445e95fa0a64f8ee
AppleJaxxZ <smackanalex@gmail.com>
2021-07-17 22:30:38 -0400

    added preventDefault in checkout page

--- client/src/pages/checkout.pages.jsx
+++ client/src/pages/checkout.pages.jsx
@@ -16,8 +16,8 @@ const CheckoutPage = ({ cartItems, total }) => {
   const [address, setAddress] = useState("");
 
   const stripe = useStripe();
-  const payNow = async () => {
-    // e.preventDefault();
+  const payNow = async (e) => {
+    e.preventDefault();
     const line_items = cartItems.map((items) => {
       return {
         quantity: items.quantity,
