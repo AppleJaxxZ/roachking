a0666f51fb3ef86ab02440b230662132639e63f8
Alex Zajacek <smackanalex@gmail.com>
2023-01-04 04:39:59 -0500

    added crypto-browserify

--- client/src/components/API/helpers.js
+++ client/src/components/API/helpers.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+// if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const API = "https://roachking.herokuapp.com" || "http://localhost:5000";
 
